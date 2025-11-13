6c9a94a4b1d5f280ca5f09781cf8c2e85cf25c04
Arya Kaul <arya_kaul@g.harvard.edu>
2025-11-13 16:51:31 -0500

    tinyjamv1.0.0

--- tinyjam.rb
+++ tinyjam.rb
@@ -1,20 +1,21 @@
+require "language/python/virtualenv"
 class Tinyjam < Formula
   desc "Jam 2 NPR Tiny Desks"
   homepage "https://github.com/aryakaul/tinyjam"
-  url "https://github.com/aryakaul/tinyjam/archive/refs/tags/0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "90eb7c13ea220f9220e2fa244c9b90b19bb664282b2c62852ef3db6978e87acb"
-  license "MIT"
+  url "https://files.pythonhosted.org/packages/7e/02/c6ab98aba1d399aabd61a161e2c7eaf6d9f1566367a4106ae5a6d5481c72/tinyjam-1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "7a8a509b55ea370874262a5824ccda4f5c4aef56a5b7a37abdcd0ed8fc6427c1"
+  license "Ethical License"
   head "https://github.com/aryakaul/tinyjam.git", branch: "main"
-
+  depends_on "python@3.11"
   depends_on "mpv"
   depends_on "yt-dlp"
 
   def install
-    bin.install "tinyjam"
+    virtualenv_install_with_resources
   end
 
   test do
-    assert_match "Jam to tiny desks with tinyjam", shell_output("#{bin}/tinyjam --help 2>&1")
+    assert_match "Jam to tiny desks", shell_output("#{bin}/tinyjam --help")
   end
 end
